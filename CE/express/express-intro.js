515b43147682f8aa2eeea76f095704787827cd21
Anamta <khananamta137@gmail.com>
2025-02-24 10:12:40 +0530

    fetching data from api with express

--- CE/express/express-intro.js
+++ CE/express/express-intro.js
@@ -2,15 +2,37 @@ import express from 'express';
 const app = express();
 
 app.get('/',(req,res) => {
-res.send("hello aryan");
+  res.send("hello aryan");
 });
 
-app.get('/api',(req,res) => {
-    const data = req.query;
-    res.send(`Hello Dear ${data.name} and my roll no. is ${data.rollno}`);
-    });
-const port = 3000;
+app.get('/api/:name/:age',(req,res) => {
+  try{
+    const data = req.params 
+    res.send(`Hello my name is ${data.name} and your age is ${data.age}`);
+  } catch(error){
+    console.log(error.message);
+  }
+  });
 
+app.get('/api',(req,res) => {
+  try{
+    const {name,rollno} = req.query;
+    if(!name){
+        res.send({
+            status: 404, message: " Name is required"
+        });
+        res.status(404).send("Name is must")
+    }
+    else{  
+        res.send(`Hello Dear ${name} and my roll no. is ${rollno}`);
+    }
+    
+}catch (error){
+    console.log(error);
+}
+});
+
+    const port = 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
